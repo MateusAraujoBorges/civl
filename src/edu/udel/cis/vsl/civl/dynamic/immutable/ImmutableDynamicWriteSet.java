003f0b93e8b71ff803d4075553130d7148cac3f1
ziqing <ziqing@fb995dde-84ed-4084-dfe6-e5aef3e2452c>
2017-04-19 18:47:14 +0000

    add interfaces for dynamic write set
    git-svn-id: svn://vsl.cis.udel.edu/civl/branches/loop_invariants@4185 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/dynamic/immutable/ImmutableDynamicWriteSet.java
+++ src/edu/udel/cis/vsl/civl/dynamic/immutable/ImmutableDynamicWriteSet.java
@@ -4,12 +4,7 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-import edu.udel.cis.vsl.civl.library.civlc.LibcivlcExecutor;
-import edu.udel.cis.vsl.civl.library.mpi.LibmpiExecutor;
-import edu.udel.cis.vsl.civl.library.time.LibtimeExecutor;
-import edu.udel.cis.vsl.civl.model.IF.variable.Variable;
-import edu.udel.cis.vsl.civl.semantics.common.CommonExecutor;
-import edu.udel.cis.vsl.civl.state.common.immutable.ImmutableStateFactory;
+import edu.udel.cis.vsl.civl.dynamic.IF.DynamicWriteSet;
 import edu.udel.cis.vsl.sarl.IF.Reasoner;
 import edu.udel.cis.vsl.sarl.IF.SymbolicUniverse;
 import edu.udel.cis.vsl.sarl.IF.UnaryOperator;
@@ -17,41 +12,11 @@ import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression.SymbolicOperator;
 
 /**
- * <p>
- * This class represents a write set value which is formed dynamically. A
- * dynamic write set stores a set of memory location references which refer to
- * the memory locations that are changed ( from a point that starts monitoring
- * "write" operations).
- * 
- * An instance of this class is a referenced value for a {@link Variable} with
- * $mem type.
- *
- * </p>
- * 
- * <p>
- * <b>Here is just a record of Java methods, calling of which will change the
- * write set</b>
- * <ul>
- * <li>The proctected method {@link CommonExecutor#assign}s, there are two of
- * them.</li>
- * <li>
- * {@link ImmutableStateFactory#deallocate(edu.udel.cis.vsl.civl.state.IF.State, SymbolicExpression, int, int, int)}
- * </li>
- * <li>
- * {@link ImmutableStateFactory#malloc(edu.udel.cis.vsl.civl.state.IF.State, int, int, SymbolicExpression)}
- * </li> TODO: can all the following go through the executor's assign() ?
- * <li>{@link LibmpiExecutor#executeNewGcomm} (TODO: is this needed to be
- * recorded ?)</li>
- * <li>{@link LibtimeExecutor#executeLocalTime}</li>
- * <li>{@link CommonExecutor#executeNextInDomain} (TODO: this one needs some
- * special non-concretet handling)</li>
- * <li>{@link LibcivlcExecutor#executeNextTimeCount}</li>
- * </ul>
- * </p>
+ * An immutable pattern implementaion of {@Link DynamicWriteSet}
  * 
  * @author ziqing (Ziqing Luo)
  */
-public class ImmutableDynamicWriteSet implements Iterable<SymbolicExpression> {
+public class ImmutableDynamicWriteSet implements DynamicWriteSet {
 	/**
 	 * A collection of references (pointers) to memroy locations (objects). For
 	 * each reference, there is an initial value associates to it.
@@ -74,18 +39,8 @@ public class ImmutableDynamicWriteSet implements Iterable<SymbolicExpression> {
 		this.pointerSet = new TreeSet<>(references);
 	}
 
-	// *********************** public methods: ***********************
-	/**
-	 * <p>
-	 * Add a set of memory location references to the write set
-	 * </p>
-	 * 
-	 * @param pointer
-	 *            A set of {@link SymbolicExpression} which represents a set of
-	 *            concrete pointer.
-	 * @return A new instance if the pointer is not in this write set; otherwise
-	 *         return this instance.
-	 */
+	/* ***************** public methods from DynamicWriteSet *****************/
+	@Override
 	public ImmutableDynamicWriteSet addReference(SymbolicExpression pointer) {
 		assert pointer.operator() == SymbolicOperator.TUPLE;
 		if (pointerSet.contains(pointer)) {
@@ -99,15 +54,7 @@ public class ImmutableDynamicWriteSet implements Iterable<SymbolicExpression> {
 		}
 	}
 
-	/**
-	 * <p>
-	 * Canonicalize all memory location references in this write set.
-	 * </p>
-	 * 
-	 * @param universe
-	 *            An instance of {@link SymbolicUniverse}
-	 * @return A new instance whose references are canonic ones of this.
-	 */
+	@Override
 	public ImmutableDynamicWriteSet canonicalize(SymbolicUniverse universe) {
 		if (canonicalized)
 			return this;
@@ -122,16 +69,7 @@ public class ImmutableDynamicWriteSet implements Iterable<SymbolicExpression> {
 
 	}
 
-	/**
-	 * <p>
-	 * Apply an {@link UnaryOperator} on the set of memory location references.
-	 * If the operator changes nothing, return this instance.
-	 * </p>
-	 * 
-	 * @param operator
-	 * @return A new instance whose references are obtained by applying the
-	 *         operator on ones of this
-	 */
+	@Override
 	public ImmutableDynamicWriteSet apply(
 			UnaryOperator<SymbolicExpression> operator) {
 		ImmutableDynamicWriteSet newSet = new ImmutableDynamicWriteSet(
@@ -151,16 +89,7 @@ public class ImmutableDynamicWriteSet implements Iterable<SymbolicExpression> {
 			return this;
 	}
 
-	/**
-	 * <p>
-	 * Simplify the reference set using the given {@link Reasoner}.
-	 * </p>
-	 * 
-	 * @param reasoner
-	 *            An instance of a {@link Reasoner}.
-	 * @return A new {@link ImmutableDynamicWriteSet} instance whose symbolic
-	 *         contents are simplified.
-	 */
+	@Override
 	public ImmutableDynamicWriteSet simplify(Reasoner reasoner) {
 		ImmutableDynamicWriteSet newSet = new ImmutableDynamicWriteSet(
 				pointerSet.comparator());
