3fc8603e4edd4ac80c494c656cb77b13d4a0574e
dxu <dxu@fb995dde-84ed-4084-dfe6-e5aef3e2452c>
2017-07-28 20:23:18 +0000

    git-svn-id: svn://vsl.cis.udel.edu/civl/branches/civl_omp@4444 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/dev/edu/udel/cis/vsl/civl/OmpDataracebenchTest.java
+++ test/dev/edu/udel/cis/vsl/civl/OmpDataracebenchTest.java
@@ -22,6 +22,7 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
+@Ignore
 public class OmpDataracebenchTest {
 
 	/* *************************** Static Fields *************************** */
