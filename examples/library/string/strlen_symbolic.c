2b84f2947f2fcbe0cd2654ad9a9e55dcd62a81ae
wuwenhao <wuwenhao@fb995dde-84ed-4084-dfe6-e5aef3e2452c>
2018-04-04 21:46:05 +0000

    Add several examples for strlen and power simplification
    git-svn-id: svn://vsl.cis.udel.edu/civl/branches/civl_tkt_wwu@4698 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/library/string/strlen_symbolic.c
+++ examples/library/string/strlen_symbolic.c
@@ -3,10 +3,10 @@
 
 $input int LENGTH;
 $assume(0 < LENGTH);
-$input char STR_IN[LENGTH];
+$input char STR_IN[2][LENGTH];
 
 void main(){
-  int len = strlen(STR_IN);
+  int len = strlen(&STR_IN[1][1]);
   
-  $assert(0 <= len && len <= LENGTH);
+  $assert(0 <= len);
 }
