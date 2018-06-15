b419a1e0575fe77c0aa801dbb448003a114d3db8
wuwenhao <wuwenhao@fb995dde-84ed-4084-dfe6-e5aef3e2452c>
2018-06-15 15:39:46 +0000

    Update two examples
    git-svn-id: svn://vsl.cis.udel.edu/civl/branches/civl_1.17_wwu@4887 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/omp/simple/omp_reduce.c
+++ examples/omp/simple/omp_reduce.c
@@ -1,8 +1,10 @@
-#include <civlc.cvh>
+#include <assert.h>
+#include <omp.h>
+#include <stdio.h>
 
 int main () {
   int n = 5;
-  int arr[n] = {5,3,9,1,7};
+  int arr[5] = {5,3,9,1,7};
   // Reduction combiners
   int max = -10;
   int min = 10;
@@ -15,53 +17,53 @@ int main () {
   int bor = (arr[0] & 0);
   int bxor = (arr[0] & 0);
 
-#pragma omp parallel for reduction(max:max)
+#pragma omp parallel for reduction(max:max) 
   for (int i=0; i<n; i++)
     max = max < arr[i] ? arr[i] : max;
-  $assert(max == 9);
+  assert(max == 9);
 
 #pragma omp parallel for reduction(min:min)
   for (int i=0; i<n; i++)
     min = min > arr[i] ? arr[i] : min;
-  $assert(min == 1);
+  assert(min == 1);
   
 #pragma omp parallel for reduction(+:sum)
   for (int i=0; i<n; i++)
     sum = sum + arr[i];
-  $assert(sum == 25);
+  assert(sum == 25);
   
 #pragma omp parallel for reduction(-:sub)
   for (int i=0; i<n; i++)
     sub = sub - arr[i];
-  $assert(sub == 5);
+  assert(sub == 5);
   
 #pragma omp parallel for reduction(*:prod)
   for (int i=0; i<n; i++)
     prod = prod * arr[i];
-  $assert(prod == 945);
+  assert(prod == 945);
   
 #pragma omp parallel for reduction(&&:land)
   for (int i=0; i<n; i++)
     land = land && arr[i];
-  $assert(land);
+  assert(land);
   
 #pragma omp parallel for reduction(&:band)
   for (int i=0; i<n; i++)
     band = band & arr[i];
-  $assert(band == 1);
+  assert(band == 1);
   
 #pragma omp parallel for reduction(||:lor)
   for (int i=0; i<n; i++)
     lor = lor || arr[i];
-  $assert(lor);
+  assert(lor);
   
 #pragma omp parallel for reduction(|:bor)
   for (int i=0; i<n; i++)
     bor = bor | arr[i];
-  $assert(bor == 15);
+  assert(bor == 15);
   
 #pragma omp parallel for reduction(^:bxor)
   for (int i=0; i<n; i++)
     bxor = bxor ^ arr[i];
-  $assert(bxor == 9);
+  assert(bxor == 9);
 }
